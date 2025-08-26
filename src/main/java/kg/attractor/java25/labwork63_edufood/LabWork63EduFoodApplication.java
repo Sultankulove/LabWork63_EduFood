b736ea2651bba743d2699603da44585b9a597140
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-26 19:10:40 +0600

    create project

---
+++ src/main/java/kg/attractor/java25/labwork63_edufood/LabWork63EduFoodApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.java25.labwork63_edufood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabWork63EduFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabWork63EduFoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/attractor/java25/labwork63_edufood/LabWork63EduFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.java25.labwork63_edufood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabWork63EduFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
