9578e3594992de0f89fdcc75712fcc789c53abef
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-28 19:17:18 +0600

    created repositories

---
+++ src/main/java/kg/attractor/java25/labwork63_edufood/repo/UserRepo.java
@@ -0,0 +1,13 @@
+package kg.attractor.java25.labwork63_edufood.repo;
+
+import kg.attractor.java25.labwork63_edufood.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
