a39fd44293a45cbb68cad2daa1b156443535d580
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-28 19:22:18 +0600

    created user model

--- src/main/java/kg/attractor/java25/labwork63_edufood/model/User.java
+++ src/main/java/kg/attractor/java25/labwork63_edufood/model/User.java
@@ -1,17 +1,36 @@
 package kg.attractor.java25.labwork63_edufood.model;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
 @Table(name = "Users")
 public class User {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "user_name")
+    private String username;
 
-    private String name;
+    @Column(name = "email", unique = true)
+    private String email;
 
+    @Column(name = "password")
     private String password;
 
+    @Column(name = "enabled")
     private boolean enabled;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> order;
 }
