e268752584bfffe97828cdbeafbf4f1aa37ed6b6
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-28 19:16:55 +0600

    created auth service

---
+++ src/main/java/kg/attractor/java25/labwork63_edufood/service/AuthService.java
@@ -0,0 +1,7 @@
+package kg.attractor.java25.labwork63_edufood.service;
+
+import kg.attractor.java25.labwork63_edufood.dto.RegisterDto;
+
+public interface AuthService {
+    void registerUser(RegisterDto register);
+}
