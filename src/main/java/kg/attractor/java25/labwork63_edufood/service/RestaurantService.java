df5439da947be45b415498769d6ebf83252f49d4
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-28 19:22:50 +0600

    created RestaurantService.java

---
+++ src/main/java/kg/attractor/java25/labwork63_edufood/service/RestaurantService.java
@@ -0,0 +1,16 @@
+package kg.attractor.java25.labwork63_edufood.service;
+
+import kg.attractor.java25.labwork63_edufood.dto.RestaurantDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface RestaurantService {
+    Page<RestaurantDto> getRestaurants(PageRequest of);
+
+    RestaurantDto getById(Long id);
+
+    List<RestaurantDto> searchByName(String name, Pageable pageable);
+}
